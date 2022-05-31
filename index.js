aae0d7434d9565c297cdec5aa02e7360da4eab4b
Aline Mozer <alinemozer@gmail.com>
2022-05-31 10:57:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function soma (primeiroValor, segundoValor) {
+    return primeiroValor + segundoValor
+}
+
+soma (10, 5)
+
+let resultadoSoma = soma (10, 5)
+console.log (resultadoSoma);
+
+function divisao (primeiroValor, segundoValor) {
+    return primeiroValor / segundoValor
+}
+
+divisao (20, 2)
+
+let resultadoDivisao = divisao (20, 2)
+console.log (resultadoDivisao);
+
+function multiplicacao (primeiroValor, segundoValor, terceiroValor) {
+    return primeiroValor * segundoValor * terceiroValor
+}
+
+multiplicacao (5, 2, 4)
+
+let resultadoMultiplicacao = multiplicacao (5, 2, 4)
+console.log (resultadoMultiplicacao);
+
+function subtracao (primeiroValor, segundoValor) {
+    return primeiroValor - segundoValor
+}
+
+subtracao (50, 20)
+
+let resultadoSubtracao = subtracao (50, 20)
+console.log (resultadoSubtracao);
